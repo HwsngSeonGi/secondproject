0abde435e31eed6d7961c4a51041b7a280265970
HwsngSeonGi <sksk8814@naver.com>
2019-04-15 01:17:24 +0900

    second commit

--- blogapp/views.py
+++ blogapp/views.py
@@ -3,37 +3,37 @@ from django.utils import timezone
 from .models import Blog
 
 def home(request):
-    빈칸 = Blog.objects
-    return render(request, 'home.html', {"posts": 빈칸})
+    posts = Blog.objects
+    return render(request, 'home.html', {"posts": posts})
 
-def detail(request, a):
-    빈칸 = get_object_or_404(Blog, pk=빈칸)
-    return render(request, 'detail.html', {빈칸: post})
+def detail(request, post_id):
+    post = get_object_or_404(Blog, pk=post_id)
+    return render(request, 'detail.html', {'post': post})
 
 def new(request):
     return render(request, 'new.html')
 
 def create(request):
     blog = Blog()
-    blog.title = request.POST[빈칸]
-    blog.body = request.POST[빈칸]
+    blog.title = request.POST['header']
+    blog.body = request.POST['section']
     blog.pub_date = timezone.datetime.now()
     blog.save()
     return redirect('/blog/'+str(blog.id))
 
-def delete(request, b):
-    destroy = get_object_or_404(Blog, pk=빈칸)
+def delete(request, post_id):
+    destroy = get_object_or_404(Blog, pk=post_id)
     destroy.delete()
     return redirect('home')
 
-def update(request, c):
-    빈칸 = get_object_or_404(Blog, pk=빈칸)
-    return render(request, 'update.html', {빈칸: text})
+def update(request, post_id):
+    new_text = get_object_or_404(Blog, pk=post_id)
+    return render(request, 'update.html', {'new_text': new_text})
 
-def edit(request, d):
-    edit = Blog.objects.get(pk=빈칸)
-    edit.title = request.POST[빈칸]
-    edit.body = request.POST[빈칸]
+def edit(request, post_id):
+    edit = Blog.objects.get(pk=post_id)
+    edit.title = request.POST['header']
+    edit.body = request.POST['section']
     edit.pub_date = timezone.datetime.now()
     edit.save()
     return redirect('home')
